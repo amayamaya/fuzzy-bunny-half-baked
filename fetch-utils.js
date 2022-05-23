bc1c7d1dca3cf8af7ed4515c74be38ae6a06235f
‘AMAYA <aamaya547@gmail.com>
2022-05-23 15:29:47 -0700

    added display function, added event listener for add bunny, bunnies deleting

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://yhletbviclmzgnzikzpu.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlobGV0YnZpY2xtemduemlrenB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTUzMjIsImV4cCI6MTk2Nzg3MTMyMn0.NSJ1Tw-LlfLsXJF4Y_nS3pwZV2TB7Ru8jvAsLxQLJWg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,13 +10,23 @@ export function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
-
-    return checkError(response);
+    const response = await client.from('loving_families').select('*, fuzzy_bunnies(*)');
+    // clear out the familiesEl
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return checkError(response);
+    }
 }
 
 export async function deleteBunny(id) {
     // delete a single bunny using the id argument
-
+    const response = await client.from('fuzzy_bunnies').delete().eq('id', id);
+    if (response.error) {
+        console.error(response.error.message);
+    } else {
+        return response.data;
+    }
     return checkError(response);
 }
 
